9087f63ca5b5800c68851e6107fb12037fb15b9b
+ <+>
2023-04-11 15:00:16 -0400

    added validations and active record associations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+    validates :name, :address, :website_url, presence: true
+    validates :phone_number, length: { is: 10 }
+    
+    has_many :reservations
+    has_many :patrons, through: :reservations
 end
