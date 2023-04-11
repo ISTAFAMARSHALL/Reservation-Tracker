ac42167d2585c991b3b98e08787af996cb6f7b9a
+ <+>
2023-04-11 16:12:24 -0400

    added Faker seed generators for Patron and Restaurant

--- db/migrate/20230411173845_create_restaurants.rb
+++ db/migrate/20230411173845_create_restaurants.rb
@@ -2,9 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string  :name
-      t.string  :address
-      t.string  :phone_number
-      t.string  :website_url
+      t.string  :type
+      t.string  :description
       t.timestamps
     end
   end
